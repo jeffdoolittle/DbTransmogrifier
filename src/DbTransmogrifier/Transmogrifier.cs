db1a6ca7bb1d78cf274632d1dbda899f63671b57
Jeff Doolittle <jeff.doolittle@lotpath.com>
2012-02-22 08:17:40 -0800

    moved project files

--- src/DbTransmogrifier/Transmogrifier.cs
+++ src/DbTransmogrifier/Transmogrifier.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Data;
+﻿using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
 using DbTransmogrifier.Config;
 using DbTransmogrifier.Dialects;
 using DbTransmogrifier.Logging;
