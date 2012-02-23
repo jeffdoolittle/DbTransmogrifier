755c655d87c399be7ec858bcd2dc6c5a23c85d29
Jeff Doolittle <jeff.doolittle@lotpath.com>
2012-02-23 12:43:48 -0800

    cleanup

--- src/DbTransmogrifier.Specs/Class1.cs
+++ src/DbTransmogrifier.Specs/Class1.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using DbTransmogrifier.Config;
+﻿using DbTransmogrifier.Config;
 using DbTransmogrifier.Dialects;
 using Moq;
 using Xunit;
@@ -30,50 +26,21 @@ namespace DbTransmogrifier.Specs
         }
     }
 
-    public class AutoMock<T>
-    {
-        private readonly ConstructorInfo _classUnderTestConstructor;
-        private IList<object> Mocks { get; set; }
+    //public abstract class DefaultMigrationResolverFixture
+    //{
+    //    protected DefaultMigrationResolverFixture()
+    //    {
+            
+    //    }
 
-        public Mock<TMock> GetMock<TMock>() where TMock : class
-        {
-            foreach (var mock in Mocks)
-            {
-                if (mock.GetType().GetGenericArguments()[0] == typeof(TMock))
-                    return (Mock<TMock>) mock;
-            }
-            return null;
-        }
+    //    protected abstract void When();
+    //}
 
-        public AutoMock()
-        {
-            var type = typeof (T);
-            _classUnderTestConstructor = type.GetConstructors()
-                .GroupBy(x => x)
-                .Select(x => new {Constructor = x.Key, ParameterCount = x.Sum(y => y.GetParameters().Count())})
-                .OrderByDescending(x => x.ParameterCount)
-                .First().Constructor;
-
-            var mocks = new List<object>();
-            var parameterInfos = _classUnderTestConstructor.GetParameters();
-            foreach (var parameterInfo in parameterInfos)
-            {
-                var mockType = typeof(Mock<>).MakeGenericType(parameterInfo.ParameterType);
-                mocks.Add(Activator.CreateInstance(mockType));
-            }
-
-            Mocks = mocks;
-        }
-
-        public T CreateClassUnderTest()
-        {
-            var mockedObjects = new List<object>();
-            foreach (dynamic mock in Mocks)
-            {
-                mockedObjects.Add(mock.Object);
-            }
-
-            return (T)_classUnderTestConstructor.Invoke(mockedObjects.ToArray());
-        }
-    }
+    //public class when_resolving_migrations : DefaultMigrationResolverFixture
+    //{
+    //    public when_resolving_migrations()
+    //    {
+            
+    //    }
+    //}
 }
