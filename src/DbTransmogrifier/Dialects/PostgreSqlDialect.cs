a886917114a79247c3b75ed3b2bf67f7a69f55fc
Jeff Doolittle <jeff.doolittle@lotpath.com>
2012-05-24 09:05:30 -0700

    added mssql dialect for tear down without dropping. separated drop into its own command.

--- src/DbTransmogrifier/Dialects/PostgreSqlDialect.cs
+++ src/DbTransmogrifier/Dialects/PostgreSqlDialect.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Common;
+﻿using System;
+using System.Data.Common;
 
 namespace DbTransmogrifier.Dialects
 {
@@ -60,6 +61,15 @@ namespace DbTransmogrifier.Dialects
         public string DeleteSchemaVersion
         {
             get { return PostgreSqlStatements.DeleteSchemaVersion; }
+        }
+
+        public string TearDown
+        {
+            get
+            {
+                throw new NotImplementedException("Tear Down not implemented for PostgreSqlDialect");
+                //return PostgreSqlStatements.TearDown;
+            }
         }
     }
 }
