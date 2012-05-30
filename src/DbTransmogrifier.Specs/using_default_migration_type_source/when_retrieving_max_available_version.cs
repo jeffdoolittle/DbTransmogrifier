aae6c6ea21fe5dc0871c10f3b150cd4e6ce3900b
Jeff Doolittle <jeff.doolittle@lotpath.com>
2012-05-30 10:02:56 -0700

    added test coverage. created build script.

---
+++ src/DbTransmogrifier.Specs/using_default_migration_type_source/when_retrieving_max_available_version.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace DbTransmogrifier.Specs.using_default_migration_type_source
+{
+    public class when_retrieving_max_available_version : DefaultMigrationTypeSourceFixture
+    {
+        [Fact]
+        public void then_value_is_non_zero()
+        {
+            Assert.NotEqual(0, ClassUnderTest.GetMaxAvailableMigrationVersion());
+        }
+    }
+}
