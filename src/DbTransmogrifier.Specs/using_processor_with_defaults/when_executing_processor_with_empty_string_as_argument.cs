9ccf651529b56a646ea6005961e0071fb1b604f0
Jeff Doolittle <jeff.doolittle@lotpath.com>
2013-04-09 16:01:25 -0700

    fixed issue with empty args array and added test coverage

---
+++ src/DbTransmogrifier.Specs/using_processor_with_defaults/when_executing_processor_with_empty_string_as_argument.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using DbTransmogrifier.Logging;
+using Xunit;
+
+namespace DbTransmogrifier.Specs.using_processor_with_defaults
+{
+    public class when_executing_processor_with_empty_string_as_argument : ProcessorFixture
+    {
+        protected override string[] ArrangeArgs()
+        {
+            return new[] { "" };
+        }
+
+        [Fact]
+        public void then_error_is_displayed()
+        {
+            var lastLog = LogMessages.Last();
+            Assert.Equal(LogLevel.Error, lastLog.Item1);
+        }
+    }
+}
